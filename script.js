c14bfec71149f46778ae9ebc654aa04e74f115e0
Michael AGUE <aguekevinmichael@gmail.com>
2026-06-22 05:16:21 +0200

    Premier commit de mon site

---
+++ script.js
@@ -0,0 +1,14 @@
+// Menu burger
+function toggleMenu() {
+  document.querySelector('.menu').classList.toggle('show');
+}
+
+// Lightbox
+function openLightbox(src) {
+  document.getElementById('lightbox').style.display = 'flex';
+  document.getElementById('lightbox-img').src = src;
+}
+
+function closeLightbox() {
+  document.getElementById('lightbox').style.display = 'none';
+}
